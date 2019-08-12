e0708a36d9ac01f1c6c206aa8ef9c4838292711f
mohan145 <mohankrishna@ibhubs.co>
2019-08-12 16:19:44 +0530

    Test Attributes of Complex Number:GreenState

--- complex_number.py
+++ complex_number.py
@@ -1,5 +1,5 @@
 class ComplexNumber:
 
     def __init__(self, real_value, imaginary_value):
-        self.real_value = 0
-        self.imaginary_value = 0
+        self.real_value = real_value
+        self.imaginary_value = imaginary_value
