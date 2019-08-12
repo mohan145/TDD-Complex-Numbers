6da56aaefd140c676e58abcafdd97b7c0a8f6d68
mohan145 <mohankrishna@ibhubs.co>
2019-08-12 16:19:21 +0530

    Test Attributes of Complex Number:RedState

--- test_complex_number.py
+++ test_complex_number.py
@@ -1,7 +1,16 @@
 from complex_number import ComplexNumber
 
 
-def test_attributes_class_complex_number():
+def test_class_complex_number():
     complex_number = ComplexNumber(real_value=0, imaginary_value=0)
     complex_number.real_value == 0
     complex_number.imaginary_value == 0
+
+
+def test_complex_number_attributes():
+    complex_number = ComplexNumber(real_value=2, imaginary_value=3)
+    assert complex_number.real_value == 2
+    assert complex_number.imaginary_value == 3
+
+
+
